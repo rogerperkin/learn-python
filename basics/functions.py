d61c03b821406b28f6e4c3fc8de730addf4128a8
Roger <roger@rogerperkin.co.uk>
2020-10-03 17:41:33 -0400

    added basics folder

---
+++ basics/functions.py
@@ -0,0 +1,15 @@
+#def my_function(): 
+#   print("Hello from a funcion")
+
+#my_function()
+
+def my_function(fname):
+    print(fname + " Refsnes")
+
+# This is a function with an argument - the argument is called fname, you 
+# can add more arguments if you separate them with a comma
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
